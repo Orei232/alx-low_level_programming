e21a1a40f1bae77be7abb710e7262de6251a38bf
Ore1232 <oreoluwa59keripe@gmail.com>
2022-10-13 08:18:20 -0700

    C pjt

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("Size of a char: %i byte\n", size of(char));
+	printf("Size of a int: %i byte\n", size of(int));
+	printf("Size of a long int: %i byte\n", size of(long int));
+	printf("Size of a long long int: %i byte\n", size of(long long int));
+	printf("Size of a float: %i byte\n", size of(float));
+	return (0);
+}
