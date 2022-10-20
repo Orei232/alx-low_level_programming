ed318ecf9ae81ce2fa16110c5d1411113d7ff4cd
Ore1232 <oreoluwa59keripe@gmail.com>
2022-10-20 08:28:50 -0700

    c

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _isupper(int c);
+int _isdigit(int c);
+int _mul(int a, int b);
+void _print_numbers(void);
+void _print_most_numbers(void);
+void _more_numbers(void);
+void _print_line(int n);
+void _print_digonal(int n);
+void _print_square(int size);
+void _print_triangle(int size);
+
+
+#endif
