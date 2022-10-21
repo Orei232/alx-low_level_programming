943af5877adc25e8486cfe22c7e48a3cf14f37f5
Ore1232 <oreoluwa59keripe@gmail.com>
2022-10-21 04:25:42 -0700

    c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,14 +3,14 @@
 
 int _isupper(int c);
 int _isdigit(int c);
-int _mul(int a, int b);
-void _print_numbers(void);
-void _print_most_numbers(void);
-void _more_numbers(void);
-void _print_line(int n);
-void _print_digonal(int n);
-void _print_square(int size);
-void _print_triangle(int size);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_digonal(int n);
+void print_square(int size);
+void print_triangle(int size);
 
 
 #endif
